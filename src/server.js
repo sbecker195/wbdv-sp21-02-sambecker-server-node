6cf3caf8d344b1d3e02a66dcc15c229cfd285ad5
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-12 17:17:46 -0400

    Push to heroku

--- src/server.js
+++ src/server.js
@@ -14,4 +14,4 @@ app.use(function (req, res, next) {
 require('./controllers/quizzes-controller')(app)
 require('./controllers/question-controller')(app)
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
