8c8d19b137fbeb0a8103304b5c08d793f536dd5d
Samantha Becker <Sam@MacBook-Pro.local>
2021-04-17 16:40:01 -0400

    Edit

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,8 +1,13 @@
 const questionsModel = require('../models/questions/questions-model')
-const quizzesModel = require('../models/quizzes/quizzes-model')
 
 const findAllQuestions = () => questionsModel.find()
-const findQuestionById = (qid) => questionsModel.findById(qid)
-const findQuestionsForQuiz = (qzid) => quizzesModel.findById(qzid)
-    .populate('questions').then(quiz => quiz.questions)
-module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
+const findQuestionById = (id) => questionsModel.findById(id)
+const findQuestionsForQuiz = (qid) => {
+    return questionsModel.find({'quizId': qid})
+}
+
+module.exports = {
+    findAllQuestions,
+    findQuestionById,
+    findQuestionsForQuiz
+}
